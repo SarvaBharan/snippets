db2cf32bcfdab8de2f395f1d48171119780f52e4
SarvaBharan <chethanraju19@gmail.com>
2023-07-09 17:04:56 +0530

    🚧 Map to human readable

--- formatDuration.js
+++ formatDuration.js
@@ -1,13 +1,20 @@
 function formatDuration(ms = 60000) {
   if (ms < 0) ms = -ms;
-  const time = {
+  let res = "";
+  let time = {
     day: Math.floor(ms / 86400000),
     hour: Math.floor(ms / 3600000) % 24,
     minute: Math.floor(ms / 60000) % 60,
     second: Math.floor(ms / 1000) % 60,
     millisecond: Math.floor(ms) % 1000,
   };
-  return time;
+  time = Object.entries(time);
+  for ([key, value] of time) {
+    if (value !== 0) {
+      res += `${value} ${key} `;
+    }
+  }
+  return res;
 }
 
 console.log(formatDuration(10240000));
