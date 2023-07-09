d2768b65bfead003395fd3d089fbb72de4d4bb0b
SarvaBharan <chethanraju19@gmail.com>
2023-07-09 16:44:04 +0530

    ✨ Format date
    WIP

---
+++ formatDuration.js
@@ -0,0 +1,12 @@
+function formatDuration(ms = 60000000) {
+  const time = {
+    day: Math.floor(ms / 86400000),
+    hour: Math.floor(ms / 3600000) % 24,
+    minute: Math.floor(ms / 60000) % 60,
+    second: Math.floor(ms / 1000) % 60,
+    millisecond: Math.floor(ms) % 1000,
+  };
+  return time;
+}
+
+console.log(formatDuration());
