f993418e00eca6f3edfcb425e3c2255cb42ac1a0
SarvaBharan <chethanraju19@gmail.com>
2023-07-09 17:17:18 +0530

    ⚡️ Working code

--- formatDuration.js
+++ formatDuration.js
@@ -1,13 +1,14 @@
 function formatDuration(ms = 60000) {
   if (ms < 0) ms = -ms;
-  let res = "";
-  let time = {
+  const time = {
     day: Math.floor(ms / 86400000),
     hour: Math.floor(ms / 3600000) % 24,
     minute: Math.floor(ms / 60000) % 60,
     second: Math.floor(ms / 1000) % 60,
     millisecond: Math.floor(ms) % 1000,
   };
+  /* // Method #1
+  let res = "";
   time = Object.entries(time);
   for ([key, value] of time) {
     if (value !== 0) {
@@ -15,6 +16,12 @@ function formatDuration(ms = 60000) {
     }
   }
   return res;
+  */
+  //Method#2
+  return Object.entries(time)
+    .filter((value) => value[1] !== 0)
+    .map(([key, value]) => `${value} ${key}${value > 1 ? "s" : ""}`)
+    .join(", ");
 }
 
 console.log(formatDuration(10240000));
