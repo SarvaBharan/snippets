d95bff4d7b31556aedd08f1bdd18de22bf2e86df
SarvaBharan <chethanraju19@gmail.com>
2023-07-10 00:12:09 +0530

    🐛 Handle edge case

--- recurringChar.js
+++ recurringChar.js
@@ -1,7 +1,11 @@
+/* This problem was asked by Google.
+Given a string, return the first recurring character in it, or null if there is no recurring character.
+For example, given the string "acbbac", return "b". Given the string "abcdef", return null. */
+
 const str = "acbbac";
 
 const recurringChar = (s) => {
-  return [...s].filter((ch, i) => ch == s[i + 1])[0].toString();
+  return [...s].filter((ch, i) => ch == s[i + 1])[0] || null;
 };
 
 console.log(recurringChar(str));
