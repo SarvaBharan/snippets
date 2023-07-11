c18761f31ba7ac5a1e04e8704b105db68ea7ad6d
SarvaBharan <chethanraju19@gmail.com>
2023-07-11 12:46:09 +0530

    📝 Add function comment

--- promisify.js
+++ promisify.js
@@ -1,3 +1,6 @@
+/* The `promisify` function is a utility function that takes a callback-based function as input
+and returns a new function that returns a promise. This allows you to use the callback-based
+function in a more convenient and readable way with promises. */
 const promisify = (fn) =>
   function (...args) {
     return new Promise((resolve, reject) => {
