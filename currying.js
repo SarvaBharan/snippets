e61cc4575fd98e22be5301bf29e6d194f638053b
SarvaBharan <chethanraju19@gmail.com>
2023-07-11 12:32:42 +0530

    📝 Add comment

--- currying.js
+++ currying.js
@@ -1,3 +1,12 @@
+/**
+ * The currying function allows you to partially apply arguments to a function and return a new
+ * function that takes the remaining arguments.
+ * @param fn - The `fn` parameter is a function that we want to curry.
+ * @param args - The `args` parameter is a rest parameter that allows the function `currying` to accept
+ * any number of arguments. These arguments will be stored in an array called `args`.
+ * @returns The currying function returns a new function that takes additional arguments and calls the
+ * original function with the combined arguments.
+ */
 function currying(fn, ...args) {
   return function (...arg) {
     return fn(...args, ...arg);
