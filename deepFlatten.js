d91bd5d6989bccb0f4ac4d4d3bd97e309804abb4
SarvaBharan <chethanraju19@gmail.com>
2023-07-20 21:13:39 +0530

    🚧 Comment

--- deepFlatten.js
+++ deepFlatten.js
@@ -1,4 +1,7 @@
+// Method 1:
 const deepFlatten = (arr) =>
   [].concat(...arr.map((ele) => (Array.isArray(ele) ? deepFlatten(ele) : ele)));
+
+// Method 2:
 
 deepFlatten([1, [2], [[3], 4], 5]); // [1,2,3,4,5]
