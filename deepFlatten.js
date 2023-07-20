9b7fce041cbf7e0fd766f342d3a4fce9d5f09566
SarvaBharan <chethanraju19@gmail.com>
2023-07-20 21:17:18 +0530

    ✨ Method 2 using reduce

--- deepFlatten.js
+++ deepFlatten.js
@@ -1,7 +1,12 @@
 // Method 1:
-const deepFlatten = (arr) =>
+/* const deepFlatten = (arr) =>
   [].concat(...arr.map((ele) => (Array.isArray(ele) ? deepFlatten(ele) : ele)));
+*/
 
 // Method 2:
-
-deepFlatten([1, [2], [[3], 4], 5]); // [1,2,3,4,5]
+const deepFlatten = (arr) =>
+  arr.reduce(
+    (res, ele) => res.concat(!Array.isArray(ele) ? ele : deepFlatten(ele)),
+    []
+  );
+console.log(deepFlatten([1, [2], [[3], 4], 5])); // [1,2,3,4,5]
