e16d3075592091f30ac3f47cf25bbf961d2aee9b
SarvaBharan <chethanraju19@gmail.com>
2023-07-21 14:50:51 +0530

    🎨 More readable code

--- formatDuration.js
+++ formatDuration.js
@@ -1,9 +1,9 @@
 function formatDuration(ms = 60000) {
   if (ms < 0) ms = -ms;
   const time = {
-    day: Math.floor(ms / 86400000),
-    hour: Math.floor(ms / 3600000) % 24,
-    minute: Math.floor(ms / 60000) % 60,
+    day: Math.floor(ms / (1000 * 60 * 60 * 24)),
+    hour: Math.floor(ms / (1000 * 60 * 60)) % 24,
+    minute: Math.floor(ms / (1000 * 60)) % 60,
     second: Math.floor(ms / 1000) % 60,
     millisecond: Math.floor(ms) % 1000,
   };
