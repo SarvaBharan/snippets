fbcb802ec51ed4072339bed7b4ab9cb08703dbb2
SarvaBharan <chethanraju19@gmail.com>
2023-07-21 09:54:24 +0530

    🔥 Remove console log

--- delay.js
+++ delay.js
@@ -1,11 +1,9 @@
 const delay = (fn, wait, ...args) => setTimeout(fn, wait, ...args);
 
-console.log(
-  delay(
-    function (input) {
-      console.log(input);
-    },
-    5000,
-    "Done"
-  )
+delay(
+  function (input) {
+    console.log(input);
+  },
+  3000,
+  "Done"
 );
