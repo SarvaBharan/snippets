a5eecb3a72f6ba2551408f09f16f518578802d03
SarvaBharan <chethanraju19@gmail.com>
2023-07-23 21:44:09 +0530

    📝 Add comment

--- chunkArray.js
+++ chunkArray.js
@@ -1,3 +1,13 @@
+/**
+ * The `chunk` function takes an array and a size as input and returns a new array with subarrays of
+ * the specified size.
+ * @param arr - The `arr` parameter is an array that we want to split into smaller chunks.
+ * @param size - The `size` parameter in the `chunk` function represents the desired size of each chunk
+ * or subarray. It determines how many elements from the original array should be grouped together in
+ * each chunk.
+ * @returns The `chunk` function returns an array of arrays, where each inner array contains `size`
+ * number of elements from the original `arr`.
+ */
 const chunk = (arr, size) => {
   return arr.reduce((resArr, ele) => {
     const lastChunk = resArr[resArr.length - 1];
