d92c8d1901b5f8142a4695a562fb2c6b57a41666
SarvaBharan <chethanraju19@gmail.com>
2023-07-23 21:48:23 +0530

    👔 Using slice and push

--- chunkArray.js
+++ chunkArray.js
@@ -21,6 +21,11 @@ const chunk = (arr, size) => {
   }, []);
 
   // Method 2:
+  let res = [];
+  for (let i = 0; i < arr.length; i += size) {
+    res.push(arr.slice(i, i + size));
+  }
+  return res;
 };
 
 console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 2));
