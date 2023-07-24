5297f3ba7d8405f78b41bc6a9cb404458a90b30b
SarvaBharan <chethanraju19@gmail.com>
2023-07-24 21:05:16 +0530

    ✨ Retry with backoff

---
+++ retryWithBackOff.js
@@ -0,0 +1,15 @@
+async function retryWithBackOff(
+  asyncFn,
+  attemptNum = 0,
+  maxRetries = 5,
+  base = 50
+) {
+  try {
+    return await asyncFn();
+  } catch (err) {
+    await sleep(Math.random() * (2 ** attemptNum * base));
+    console.log("attemptNum :", attemptNum);
+    if (++attemptNum > maxRetries) throw err.message;
+    return retryWithBackOff(asyncFn, attemptNum, maxRetries, base);
+  }
+}
