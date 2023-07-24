56586f215b1dd122024c2f49a95b2b4f999f9849
SarvaBharan <chethanraju19@gmail.com>
2023-07-24 21:39:15 +0530

    📝 function comment

--- retryWithBackOff.js
+++ retryWithBackOff.js
@@ -1,5 +1,21 @@
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+/**
+ * The function `retryWithBackOff` retries an asynchronous function with an increasing delay between
+ * each attempt, up to a maximum number of retries.
+ * @param asyncFn - The async function that you want to retry with backoff. This function should return
+ * a promise.
+ * @param [attemptNum=0] - The `attemptNum` parameter represents the current attempt number of the
+ * function call. It is used to keep track of the number of retries that have been made so far.
+ * @param [maxRetries=12] - The `maxRetries` parameter specifies the maximum number of times the
+ * `asyncFn` function should be retried before giving up.
+ * @param [base=50] - The base parameter is the base value for calculating the backoff delay. It is
+ * multiplied by 2 raised to the power of the attempt number to determine the delay between retries.
+ * @returns The function `retryWithBackOff` returns the result of the `asyncFn` function if it is
+ * successful. If there is an error, it will retry the `asyncFn` function with an exponential backoff
+ * delay and a maximum number of retries. If the maximum number of retries is reached, it will throw
+ * the error message.
+ */
 async function retryWithBackOff(
   asyncFn,
   attemptNum = 0,
