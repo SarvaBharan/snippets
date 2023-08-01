5c13e299d356c82f1ee01b78c4f57a1525a1b038
SarvaBharan <chethanraju19@gmail.com>
2023-08-01 23:59:45 +0530

    🚧 function desc

--- retry.js
+++ retry.js
@@ -1,4 +1,11 @@
 // ! Bug: console logs before setTimeOut - need to relook
+/**
+ * The `retry` function retries a given function a maximum number of times until a condition is met.
+ * @param fn - The `fn` parameter is a function that will be executed repeatedly until a certain
+ * condition is met.
+ * @param maxRetries - The `maxRetries` parameter is the maximum number of times the function `fn` will
+ * be retried.
+ */
 const retry = (fn, maxRetries) => {
   let condition = false;
   while (maxRetries-- > 0 && !(condition = fn())) {
