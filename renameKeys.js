bc81ca9d72e8d97adcb541b48107a4f43d0f6a0c
SarvaBharan <chethanraju19@gmail.com>
2023-08-10 23:08:25 +0530

    document

--- renameKeys.js
+++ renameKeys.js
@@ -1,3 +1,11 @@
+/**
+ * The `renameKeys` function takes in an object with key-value pairs representing renaming rules and
+ * another object, and returns a new object with keys renamed according to the rules.
+ * @param renameObj - An object that maps keys from the original object to their new names.
+ * @param obj - The `obj` parameter is an object that you want to rename the keys of.
+ * @returns The `renameKeys` function returns a new object with the keys renamed according to the
+ * `renameObj` parameter.
+ */
 const renameKeys = (renameObj, obj) => {
   return Object.keys(obj).reduce(
     (acc, key) => ({
